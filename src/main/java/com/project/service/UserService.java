be17f762dbae1029829c848cc9a9d3e775952921
Aniruddha Shivananda <aniruddhams2023@gmail.com>
2024-08-27 22:09:52 -0400

    Implement Login User API

--- src/main/java/com/project/service/UserService.java
+++ src/main/java/com/project/service/UserService.java
@@ -5,16 +5,22 @@ import com.project.service.contract.RegisterRequestObject;
 import com.project.service.persistence.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-    public UserEntity registerUser(RegisterRequestObject registerRequestObject){
+    /**
+     * @param registerRequestObject
+     * @return UserEntity
+     */
+    public UserEntity registerUser(RegisterRequestObject registerRequestObject) {
 
         UserEntity userEntity = new UserEntity();
         userEntity.setName(registerRequestObject.getUsername());
@@ -22,5 +28,22 @@ public class UserService {
         userEntity.setOnline(true);
         log.info("Created User {}", userEntity.getId());
         return userRepository.save(userEntity);
+    }
+
+    /**
+     * @param registerRequestObject
+     * @return UserEntity
+     */
+    public UserEntity loginUser(RegisterRequestObject registerRequestObject) {
+        UserEntity retrievedEntity = userRepository.findByNameAndPassword(registerRequestObject.getUsername(), registerRequestObject.getPassword());
+        //For New user Just register him to the system
+        if (retrievedEntity == null) {
+            return registerUser(registerRequestObject);
+        }
+        //Make the User Online and Update in DB
+        else {
+            retrievedEntity.setOnline(true);
+            return userRepository.save(retrievedEntity);
+        }
     }
 }
