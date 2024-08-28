a9a8ac6b68e63ba8d155947b633e344aed22c1f4
Aniruddha Shivananda <aniruddhams2023@gmail.com>
2024-08-27 22:09:52 -0400

    Implement Login User API

--- src/main/java/com/project/service/adapter/rest/PingMeApiController.java
+++ src/main/java/com/project/service/adapter/rest/PingMeApiController.java
@@ -6,8 +6,10 @@ import com.project.service.contract.RegisterResponseObject;
 import com.project.service.UserService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PingMeApiController {
 
-    private final UserService userService;
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/pingme/register")
     public ResponseEntity<RegisterResponseObject> pingMeRegister(@RequestBody RegisterRequestObject registerRequestObject) {
@@ -24,6 +27,14 @@ public class PingMeApiController {
         RegisterResponseObject registerResponseObject = new RegisterResponseObject();
         registerResponseObject.setUserId(entity.getId());
         return new ResponseEntity<>(registerResponseObject, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/pingme/login")
+    public ResponseEntity<RegisterResponseObject> pingMeLogin(@RequestBody RegisterRequestObject registerRequestObject) {
+        UserEntity entity = userService.loginUser(registerRequestObject);
+        RegisterResponseObject registerResponseObject = new RegisterResponseObject();
+        registerResponseObject.setUserId(entity.getId());
+        return new ResponseEntity<>(registerResponseObject, HttpStatus.OK);
     }
 }
 
--- src/main/java/com/project/service/persistence/UserRepository.java
+++ src/main/java/com/project/service/persistence/UserRepository.java
@@ -2,11 +2,15 @@ package com.project.service.persistence;
 
 import com.project.service.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
-
+    // Custom query to find ID by NAME and PASSWORD
+    @Query("SELECT u FROM UserEntity u WHERE u.name = :name AND u.password = :password")
+    UserEntity findByNameAndPassword(@Param("name") String name, @Param("password") String password);
 }
