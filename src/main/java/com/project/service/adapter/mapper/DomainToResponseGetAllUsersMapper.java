9a0f428e71be3bfc84d6c0f8f40c9f6fcb18c269
Aniruddha Shivananda <aniruddhams2023@gmail.com>
2024-08-30 13:35:13 -0400

    code refactoring to fix the PR comments

---
+++ src/main/java/com/project/service/adapter/mapper/DomainToResponseGetAllUsersMapper.java
@@ -0,0 +1,23 @@
+package com.project.service.adapter.mapper;
+
+import com.project.service.contract.UserResponseObject;
+import com.project.service.entity.UserEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class DomainToResponseGetAllUsersMapper {
+    public List<UserResponseObject> PingMeGetAllUsersResponseMapper(List<UserEntity> listOfOnlineUsers) {
+        return listOfOnlineUsers.stream()
+                .map(userEntity -> {
+                    UserResponseObject userResponseObject = new UserResponseObject();
+                    userResponseObject.setId(userEntity.getId().toString());
+                    userResponseObject.setName(userEntity.getName());
+                    userResponseObject.setOnline(userEntity.isOnline());
+                    return userResponseObject;
+                })
+                .collect(Collectors.toList());
+    }
+}
