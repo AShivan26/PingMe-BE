eccbc3b92356ac925e307b2c560c2c02ff992da4
Aniruddha Shivananda <aniruddhams2023@gmail.com>
2024-10-23 21:22:20 -0400

    setOnline true implementation

--- src/main/java/com/project/service/adapter/rest/PingMeApiController.java
+++ src/main/java/com/project/service/adapter/rest/PingMeApiController.java
@@ -1,18 +1,14 @@
 package com.project.service.adapter.rest;
 
-import com.project.service.adapter.mapper.DomainToResponseGetAllUsersMapper;
 import com.project.service.contract.*;
-import com.project.service.entity.UserEntity;
 import com.project.service.UserService;
 
 import com.project.service.exception.UserException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,11 +16,8 @@ import java.util.UUID;
 @RequestMapping("/auth")
 public class PingMeApiController {
 
-    private final UserService userService;
-
-    private final DomainToResponseGetAllUsersMapper domainToResponseGetAllUsersMapper;
-
-
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> pingMeRegister(@RequestBody RegisterRequestObject registerRequestObject) throws UserException {
@@ -32,29 +25,10 @@ public class PingMeApiController {
         return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
     }
 
-
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> pingMeLogin(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<AuthResponse> pingMeLogin(@RequestBody LoginRequest loginRequest) throws UserException {
         AuthResponse response = userService.loginUser(loginRequest);
         return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
-    }
-
-
-
-
-
-    //Do we need these???
-    @PostMapping("/pingme/logout/{userid}")
-    public ResponseEntity<Boolean> pingMeLogout(@PathVariable UUID userid) {
-        Boolean response = userService.logOutUser(userid);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    @GetMapping("/pingme/users/{userId}")
-    public ResponseEntity<List<UserResponseObject>> pingMeGetAllUsers(@PathVariable UUID userId) {
-        List<UserEntity> listOfOnlineUsers = userService.getAllUsers(userId);
-        List<UserResponseObject> pingMeResponseObjects = domainToResponseGetAllUsersMapper.PingMeGetAllUsersResponseMapper(listOfOnlineUsers);
-        return new ResponseEntity<>(pingMeResponseObjects, HttpStatus.OK);
     }
 }
 
