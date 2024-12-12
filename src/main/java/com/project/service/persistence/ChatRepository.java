4326711815e6619e517296985c1c789617c52b7b
Aniruddha Shivananda <aniruddhams2023@gmail.com>
2024-12-12 11:58:45 -0500

    fix h2 console

--- src/main/java/com/project/service/persistence/ChatRepository.java
+++ src/main/java/com/project/service/persistence/ChatRepository.java
@@ -12,9 +12,9 @@ import java.util.UUID;
 public interface ChatRepository extends JpaRepository<ChatEntity, UUID> {
 
     @Query("select c from ChatEntity c join c.users u where u.id=:userId")
-    public List<ChatEntity> findChatByUserId(@Param("userId") UUID userId);
+    List<ChatEntity> findChatByUserId(@Param("userId") UUID userId);
 
     @Query("select c from ChatEntity c where c.isGroup=false and :user member of c.users and :reqUser member of c.users")
-    public ChatEntity findSingleChatByUserIds(@Param("user") UserEntity user, @Param("reqUser") UserEntity reqUser);
+    ChatEntity findSingleChatByUserIds(@Param("user") UserEntity user, @Param("reqUser") UserEntity reqUser);
 
 }
