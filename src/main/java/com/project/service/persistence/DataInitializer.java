55dca180d284693568bc2410538b26d8f5ada165
AShivan26 <aniruddhams2023@gmail.com>
2024-12-24 13:40:27 -0500

    Dynamic database update of the user data

---
+++ src/main/java/com/project/service/persistence/DataInitializer.java
@@ -0,0 +1,40 @@
+package com.project.service.persistence;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostConstruct
+    public void initializeData() {
+        String password1 = passwordEncoder.encode("secret1");
+        String password2 = passwordEncoder.encode("secret2");
+        String password3 = passwordEncoder.encode("secret3");
+
+        String sql1 = "INSERT INTO person (id, name, password, online, email, profile) " +
+                "VALUES ('50b35861-d3cd-40b2-8c3b-606b673e1e50', 'Ratan', ?, false, 'test1@gmail.com', 'ratan_profile')";
+
+        String sql2 = "INSERT INTO person (id, name, password, online, email, profile) " +
+                "VALUES ('50b35861-d3cd-40b2-8c3b-606b673e1e51', 'Deepshul', ?, true, 'test2@gmail.com', 'deepshul_profile')";
+
+        String sql3 = "INSERT INTO person (id, name, password, online, email, profile) " +
+                "VALUES ('50b35861-d3cd-40b2-8c3b-606b673e1e52', 'Benak', ?, true, 'test3@gmail.com', 'benak_profile')";
+
+        jdbcTemplate.update(sql1, password1);
+        jdbcTemplate.update(sql2, password2);
+        jdbcTemplate.update(sql3, password3);
+    }
+
+}
