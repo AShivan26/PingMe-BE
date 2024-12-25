73c025cf36c175f328d96f789ab00c67f4b1664e
AShivan26 <aniruddhams2023@gmail.com>
2024-12-24 19:02:49 -0500

    Update the db

--- src/main/java/com/project/service/persistence/DataInitializer.java
+++ src/main/java/com/project/service/persistence/DataInitializer.java
@@ -21,13 +21,13 @@ public class DataInitializer {
         String password3 = passwordEncoder.encode("secret3");
 
         String sql1 = "INSERT INTO person (id, name, password, online, email, profile) " +
-                "VALUES ('50b35861-d3cd-40b2-8c3b-606b673e1e50', 'Ratan', ?, false, 'test1@gmail.com', 'ratan_profile')";
+                "VALUES ('50b35861-d3cd-40b2-8c3b-606b673e1e50', 'Michael', ?, false, 'test1@gmail.com', 'mscott_profile')";
 
         String sql2 = "INSERT INTO person (id, name, password, online, email, profile) " +
-                "VALUES ('50b35861-d3cd-40b2-8c3b-606b673e1e51', 'Deepshul', ?, true, 'test2@gmail.com', 'deepshul_profile')";
+                "VALUES ('50b35861-d3cd-40b2-8c3b-606b673e1e51', 'Toby Flenderson', ?, true, 'test2@gmail.com', 'toby_profile')";
 
         String sql3 = "INSERT INTO person (id, name, password, online, email, profile) " +
-                "VALUES ('50b35861-d3cd-40b2-8c3b-606b673e1e52', 'Benak', ?, true, 'test3@gmail.com', 'benak_profile')";
+                "VALUES ('50b35861-d3cd-40b2-8c3b-606b673e1e52', 'Andrew Bernard', ?, true, 'test3@gmail.com', 'andy_profile')";
 
         jdbcTemplate.update(sql1, password1);
         jdbcTemplate.update(sql2, password2);
